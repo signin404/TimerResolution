f4822bc31ef3c6f04b43fe4d975e4ba9ffc118d6
signin404 <145902543+signin404@users.noreply.github.com>
2025-06-30 23:37:58 +0800

    Create TimerResolutionModifier.cpp

---
+++ TimerResolutionModifier.cpp
@@ -0,0 +1,26 @@
+// TimerResolutionModifier.cpp
+#include <Windows.h>
+#include <timeapi.h>
+
+#pragma comment(lib, "Winmm.lib")
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        timeBeginPeriod(1);
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        timeEndPeriod(1);
+        break;
+    }
+    return TRUE;
+}
